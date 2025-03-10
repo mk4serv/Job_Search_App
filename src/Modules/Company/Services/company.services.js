e23de3217163fc0c11f494b1a7ba4c244ed1ec5e
Mohamed Khairy <info@mk4serv.com>
2025-03-10 13:50:51 +0200

    remove import comment for new build

--- src/Modules/Company/Services/company.services.js
+++ src/Modules/Company/Services/company.services.js
@@ -1,4 +1,4 @@
-// import cloudinary from "../../../config/cloudinary.config.js";
+import cloudinary from "../../../config/cloudinary.config.js";
 import { Company } from "../../../DB/models/index.js";
 
 
